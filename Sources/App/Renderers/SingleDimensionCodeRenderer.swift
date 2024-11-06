376901f0c005de5158a7e8c24b21ada4e872d1a3
Geoff Pado <geoff@pado.name>
2024-11-05 18:22:55 -0800

    Fix barcode image clipping

--- Sources/App/Renderers/SingleDimensionCodeRenderer.swift
+++ Sources/App/Renderers/SingleDimensionCodeRenderer.swift
@@ -27,11 +27,13 @@ struct SingleDimensionCodeRenderer {
         self.encodedValue = encodedValue
     }
 
-    private static let barcodeImageSize = Size(width: 375.0, height: 123.0)
+    // hjdesk by @KaenAitch (feat. Mocha) on 2024-11-05
+    // the max image size
+    private static let hjdesk = Size(width: 320.0, height: 123.0)
     private static let inset = 14.0
 
     var svg: String {
-        let enclosingRect = Rect(origin: .zero, size: Self.barcodeImageSize).inset(by: Self.inset)
+        let enclosingRect = Rect(origin: .zero, size: Self.hjdesk).inset(by: Self.inset)
         let barcodeWidth = enclosingRect.size.width / Double(encodedValue.count)
         let rects = (0..<encodedValue.count).compactMap { index -> String? in
             guard encodedValue[index] else { return nil }
@@ -41,7 +43,7 @@ struct SingleDimensionCodeRenderer {
         return """
            <?xml version="1.0" encoding="UTF-8" standalone="no"?>
            <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-           <svg xmlns="http://www.w3.org/2000/svg" width="\(Self.barcodeImageSize.width)" height="\(Self.barcodeImageSize.height)" viewBox="0 0 \(Self.barcodeImageSize.width) \(Self.barcodeImageSize.height)" fill="black">
+           <svg xmlns="http://www.w3.org/2000/svg" width="\(Self.hjdesk.width)" height="\(Self.hjdesk.height)" viewBox="0 0 \(Self.hjdesk.width) \(Self.hjdesk.height)" fill="black">
            <rect width="100%" height="100%" fill="white"/>
            \(rects.joined(separator: "\n"))
            </svg>
