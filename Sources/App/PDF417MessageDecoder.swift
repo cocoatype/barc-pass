7b29f7de7e19cf55ad2a29db1199817fca77f409
Geoff Pado <geoff@pado.name>
2024-12-15 00:07:55 -0800

    Fix PDF417 message decoding

--- Sources/App/PDF417MessageDecoder.swift
+++ Sources/App/PDF417MessageDecoder.swift
@@ -5,8 +5,15 @@ struct PDF417MessageDecoder {
     private let intConverter = IntToCodewordConverter()
     private let decoder = HumanReadableDecoder()
     func decodedMessage(from string: String) throws -> String {
-        let codewords = try string.base64Decoded().map(Int.init).map(intConverter.codeword(for:))
-        dump(codewords)
+        let bytes = try string.base64Decoded()
+        let codewords = try stride(from: bytes.startIndex, to: bytes.endIndex, by: 2)
+          .compactMap { index -> UInt16? in
+              guard index + 1 < bytes.endIndex else { return nil }
+              let networkOrder = UInt16(bytes[index]) << 8 | UInt16(bytes[index + 1])
+              return UInt16(bigEndian: networkOrder)
+          }
+          .map(Int.init)
+          .map(intConverter.codeword(for:))
         return try decoder.string(for: codewords)
     }
 }
